8cd2acb77d6d8b7b32c844d1bfef2482d2f0c87a
Steve <stshryu@gmail.com>
2024-07-07 16:50:58 -0400

    Starting DB connections

---
+++ server/database/database.py
@@ -0,0 +1,11 @@
+from typing import List, Union
+from beanie import PydanticObjectId
+from models.profile import Profile
+from models.food import Food
+from models.cold_brew import ColdBrew
+
+profile_collection = Profile
+food_collection = Food
+cold_brew_collection = ColdBrew
+
+
--- server/models/cold_brew.py
+++ server/models/cold_brew.py
@@ -6,13 +6,15 @@ class ColdBrew(Document):
     brew_length: int
     bean_type: str
     bean_grind: str
+    profile_id: str
 
     class Config:
         json_schema_extra = {
             "example": {
                 "brew_length": 30,
                 "bean_type": "Blank Space Coffee",
-                "bean_grind": "Fine"
+                "bean_grind": "Fine",
+                "profle_id": "abcdefg123456"
             }
         }
 
